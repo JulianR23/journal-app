f20ed3668e8a4092d648e60030a4c80a4817d2a4
Julián Clavijo <130173701+JulianR23@users.noreply.github.com>
2025-08-06 14:45:52 -0500

    feat(notes): creating and active notes

---
+++ src/store/journal/thunks.js
@@ -0,0 +1,22 @@
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { addNewEmptyNote, savingNewNote, setSaving } from "./journalSlice";
+import { FirebaseDB } from "../../firebase/config";
+
+export const startNewNote = () => {
+    return async (dispatch, getState) => {
+        dispatch(savingNewNote())
+ 
+        const { uid } = getState().auth;
+
+        const newNote = {
+            title: '',
+            body: '',
+            date: new Date().getTime(),
+        };
+
+        const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
+        await setDoc(newDoc, newNote);
+        newNote.id = newDoc.id;
+        dispatch(addNewEmptyNote(newNote)); 
+    };
+} 
